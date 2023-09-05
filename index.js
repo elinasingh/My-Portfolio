399c58e9077b91e39a3db7eed602232c22b69a36
Elina <elinasingh1997@gmail.com>
2023-09-05 21:20:40 +0530

    added eventlistener on about section

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+let tabLinks = document.querySelectorAll(".tab-links");
+let tabContents = document.querySelectorAll(".tab-contents");
 
+tabLinks.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+
+        //add and removes class on tablinks
+        tabLinks.forEach(function (item) {
+            item.classList.remove("active");
+        })
+            item.classList.add("active");
+    
+       //removes display of areas
+       tabContents.forEach(function (tab) {
+        tab.style.display="none";
+       })
+
+       //equate tab links to tab contents
+
+       let targetPanel = item.getAttribute("aria-controls");
+       document.getElementById(targetPanel).style.display = "block";
+
+}, false);
+
+});
