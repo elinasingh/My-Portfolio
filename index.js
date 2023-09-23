9f9c77b3c358d614cf58a88c2ecbefca018c5cc2
Elina <elinasingh1997@gmail.com>
2023-09-23 21:16:33 +0530

    adding links for nav ul and add form data submit to google sheets

--- index.js
+++ index.js
@@ -35,4 +35,21 @@ function closeMenu() {
   sideMenu.style.right ="-200px";
 }
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw52t4-dkeEydVBctTuueucsJ2oLqvHQH5MNPM1dWiA0ksL6RWvgxZCnIDRD0m83r85uQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message send Successfully!"
+        setTimeout(function() {
+          msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+
 
